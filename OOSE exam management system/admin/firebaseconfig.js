4c1d7a5c5e873bea1f8d0904dd76411a58f253b8
Abhijeet737 <76195239+Abhijeet737@users.noreply.github.com>
2022-04-27 23:50:33 +0530

    Add files via upload

---
+++ OOSE exam management system/admin/firebaseconfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBIWH7iWRTQEBJpQtSvYr4r0z8LYaJgGKA",
+  authDomain: "exam-management-system-3d12d.firebaseapp.com",
+  databaseURL: "https://exam-management-system-3d12d-default-rtdb.firebaseio.com",
+  projectId: "exam-management-system-3d12d",
+  storageBucket: "exam-management-system-3d12d.appspot.com",
+  messagingSenderId: "275134166912",
+  appId: "1:275134166912:web:dc0f429826e872e53fb365"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
